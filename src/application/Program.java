e3eb3ca3f4f4b1746fb6acaea21f145c36486074
Rodrigo Vanhoz Ribeiro <rodrigo.vanhoz@gmail.com>
2019-11-02 01:35:34 -0200

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,17 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import db.DB;
-import db.DbException;
-import db.DbIntegrityException;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
 		
 	}
 
